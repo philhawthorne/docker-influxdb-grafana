b9e54bf42599d04798b54f05f3f251129ed98883
Phil Hawthorne <me@philhawthorne.com>
2019-08-26 09:28:45 +1000

    Update run script for InfluxDB PID file
    We need to ensure this file is writeable when InfluxDB starts

--- run.sh
+++ run.sh
@@ -2,6 +2,13 @@
 
 # We need to ensure this directory is writeable on start of the container
 chmod 0777 /var/lib/grafana
-chown influxdb:influxdb /var/run/influxdb/influxd.pid
+
+# PID file for Influx daemon needs to be writeable
+PIDFILE=/var/run/influxdb/influxd.pid
+PIDDIR=`dirname $PIDFILE`
+if [ ! -d "$PIDDIR" ]; then
+    mkdir -p $PIDDIR
+    chown influxdb:influxdb $PIDDIR
+fi
 
 exec /usr/bin/supervisord
