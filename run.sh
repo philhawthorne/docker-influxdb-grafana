4003ba42bf2a257b5222beeee6e89bba1918056b
Phil Hawthorne <me@philhawthorne.com>
2019-08-26 09:07:03 +1000

    Make InfluxDB owned by InfluxDB user
    Attempts to fix the /var/run/influxdb/influxd.pid not writable, check permissions [ FAILED ]
    error as reported in #26

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@
 
 # We need to ensure this directory is writeable on start of the container
 chmod 0777 /var/lib/grafana
+chown influxdb:influxdb /var/run/influxdb/influxd.pid
 
 exec /usr/bin/supervisord
